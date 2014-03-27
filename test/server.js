4ed37e8ae669b04905d32af71c258de314302827
iwillwen <willwengunn@gmail.com>
2014-03-27 14:29:26 +0800

    Updated: 更新

--- test/server.js
+++ test/server.js
@@ -1,4 +1,4 @@
-var web = require('webjs');
+var http = require('http');
 var qiniu = require('node-qiniu');
 var fs = require('fs');
 var util = require('util');
@@ -12,22 +12,26 @@ var uploadPage = fs.readFileSync(__dirname + '/index.html').toString();
 var viewPage = fs.readFileSync(__dirname + '/view.html').toString();
 var qiniuJs = fs.readFileSync(__dirname + '/../qiniu.js');
 
-web.run(process.argv[2] || 8080)
-  .get({
-    '/': function(req, res) {
-      var putToken = (new qiniu.Token.PutPolicy({
-        scope: 'qiniu-sdk-test'
-      })).token();
+var bucket = qiniu.bucket('qiniu-sdk-test');
 
-      res.send(util.format(uploadPage, putToken));
-    },
-    '/view': function(req, res) {
-      var getToken = (new qiniu.Token.GetPolicy())
-        .token('http://qiniu-sdk-test.qiniudn.com/', '2.jpg');
+var server = http.createServer(function(req, res) {
+  switch (req.url) {
+    case '/':
+      var putToken = bucket.token();
 
-      res.send(util.format(viewPage, getToken));
-    },
-    '/qiniu.js': function(req, res) {
-      res.send(qiniuJs);
-    }
-  });
+      res.end(util.format(uploadPage, putToken));
+      break;
+
+    case '/view':
+      var getToken = bucket.key('2.jpg').token().token;
+
+      res.end(util.format(viewPage, getToken));
+      break;
+
+    case '/qiniu.js':
+      res.end(qiniuJs);
+      break;
+  }
+});
+
+server.listen(process.argv[2] || 8080);
